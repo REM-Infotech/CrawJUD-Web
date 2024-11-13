46c184e4d46e34b5ebf5603846e0eba1ce5278fc
Robotz213 <nicholas@robotz.dev>
2024-11-13 11:27:08 -0400

    [Refact]:  Ajustado Data na tabela

--- static/js/DataTables.js
+++ static/js/DataTables.js
@@ -3,34 +3,27 @@ window.addEventListener('DOMContentLoaded', event => {
     var datatablesSimple = document.querySelector(
         'table[id="DataTable"]');
     if (datatablesSimple) {
-        new DataTable(datatablesSimple,{
+        new DataTable(datatablesSimple, {
 
             searching: false,
             deferRender: true,
             deferLoading: 57,
             processing: true
-            
+
 
         });
     };
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     var location_url = $(location).attr('href');
 
-    if (location_url.toLowerCase().includes("dashboard") || location_url.toLowerCase().includes("executions")){
+    if (location_url.toLowerCase().includes("dashboard") || location_url.toLowerCase().includes("executions")) {
         var targets = [4, 6];
-    } else if(location_url.toLowerCase().includes("users")){
+    } else if (location_url.toLowerCase().includes("users")) {
         var targets = [4];
     };
 
-    $('#FormatedDataTable').DataTable({  
-        columnDefs: [
-            {
-                targets: targets,
-                render: DataTable.render.datetime('D/MMM/YYYY', "pt")
-            }
-        ]
-    });
+    $('#FormatedDataTable').DataTable();
 });
