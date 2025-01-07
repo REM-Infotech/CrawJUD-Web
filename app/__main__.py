8ce72e7334f23e9962644f7c5ed0a6d986c31394
Robotz213 <nicholas@robotz.dev>
2025-01-07 08:58:18 -0400

    Update main application script
    - Refactor code for better readability and maintainability
    - Fix minor bugs in the main execution flow

--- app/__main__.py
+++ app/__main__.py
@@ -33,38 +33,3 @@ if __name__ == "__main__":
             f.write(checkout_release_tag())
 
     server(listen(("localhost", port)), app, log=app.logger)
-from os import getcwd, path
-from pathlib import Path
-
-from clear import clear
-from dotenv import dotenv_values as values
-from eventlet import listen
-from eventlet.wsgi import server
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-
-    clear()
-    port = int(values().get("PORT", 5000))
-    print(
-        f"""
-=======================================================
-
-            Executando servidor Flask
-            * Porta: {int(values().get("PORT", "8000"))}
-
-=======================================================
-              """
-    )
-
-    version_Path = Path(path.join(getcwd(), ".version"))
-    if version_Path.exists() is False:
-        from app.misc.checkout import checkout_release_tag
-
-        with open(".version", "w") as f:
-            f.write(checkout_release_tag())
-
-    server(listen(("localhost", port)), app, log=app.logger)
